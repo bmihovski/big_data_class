b355db0cb44bc0230c1004b172dc5d29768d2933
bhristov <magnetgroup@live.com>
2020-01-24 23:48:32 +0200

    Map reducer fixed python 3

--- simple_word_count/mapper.py
+++ simple_word_count/mapper.py
@@ -6,7 +6,7 @@ class Mapper:
             words = line.split()
             for w in words:
                 counts[w] = counts.get(w, 0) + 1
-            for w, c in counts.items():
-                returnval.append((w, c))
-            return returnval
+        for w, c in counts.items():
+            returnval.append((w, c))
+        return returnval
 
